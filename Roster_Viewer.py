eb8801dba55a35f6e75f44b4cddaccfde565d0d0
ebalster1 <ebalster1@udayton.edu>
2025-03-03 09:28:49 -0500

    minor changes

--- Roster_Viewer.py
+++ Roster_Viewer.py
@@ -18,10 +18,17 @@ from tkinter import filedialog as fd
 
 root.title('Roster Viewer')
 root.geometry("1570x1000")
+# icon
+try:
+    img = PhotoImage(file='./_internal/Grad.png')
+    root.iconphoto(False, img)
+except:
+    print("There is no icon")
+
 # width and height of Ledger_Viewer
 RVwidth = 1570
 RVheight = 1000
-header = 50
+header = 75
 
 # add some style
 style = ttk.Style()
@@ -107,7 +114,7 @@ def resizer(event):
         # width and height of Ledger_Viewer
         RVwidth = event.width
         RVheight = event.height
-        header = 50
+        header = 75
 
         # Format our columns
         CW = int(.00318*RVwidth)
@@ -139,12 +146,21 @@ def resizer(event):
         # change the location and size of the scrollbar
         vsb.place(x=RVwidth-20, y=header, height=RVheight-header-10)  
 
+        CR_label.place(x=RVwidth-250, y=10)
+        CRbox.place(x=RVwidth-185, y=10)
+        check.place(x=RVwidth-115, y=10)
+
+        ID_label.place(x=RVwidth-250, y = 43)
+        IDbox.place(x=RVwidth-185, width=100, y=43)
+        IDbutton.place(x=RVwidth-75, y=38, width=60, height=30)
+
 def print_roster(event):
     k = 0
     child = 0
     first_row_in_class = 0
     even_odd = 0
     prev_class = ''
+    prev_student = ''
  
     students = []
     instructors = []
@@ -195,21 +211,44 @@ def print_roster(event):
     if(IDbox.get() != ''):
         drop_dept.current(0)
 
-    for row in complete_roster:
-        print_roster = False
+    # remove duplicate rows
+    modified_roster = []
+    r = 0
+    while r < len(complete_roster):
+        # if we have the same student and class, it's a duplicate record
+        if(r < len(complete_roster)-1):
+            if(complete_roster[r][13] == complete_roster[r+1][13] and complete_roster[r][2] == complete_roster[r+1][2]):
+                # column 8 is the meeting days, final exam days are duplicate records
+                countrow = sum([1 for c in complete_roster[r][8] if c.isalpha()])
+                countnextrow = sum([1 for c in complete_roster[r+1][8] if c.isalpha()])
+                if(countrow > countnextrow):
+                    modified_roster.append(complete_roster[r])
+                else:
+                    modified_roster.append(complete_roster[r+1])
+                r += 2
+            else:
+                modified_roster.append(complete_roster[r])
+                r += 1
+        else:
+            modified_roster.append(complete_roster[r])
+            r += 1
+
+    CRs = 0
+    for row in modified_roster:
+        print_r = False
         if(drop_dept.get() == '' and drop_student.get() == '' and 
            drop_instruct.get() == '' and IDbox.get() == ''):
-            print_roster = True
+            print_r = True
         elif(drop_dept.get() == row[1] and drop_student.get() == '' and drop_instruct.get() == ''):
-            print_roster = True
+            print_r = True
         elif(drop_instruct.get() == row[4]):
-            print_roster = True
+            print_r = True
         elif(drop_student.get() == row[13]):
-            print_roster = True
+            print_r = True
         elif(row[12] in IDbox.get() and IDbox.get() != ''):
-            print_roster = True
-
-        if(print_roster == True):
+            print_r = True
+ 
+        if(print_r == True):
             # get the course number
             num = re.findall(r'\d+', row[2])
             if((locale.atof(row[11]) < 10) and (locale.atof(num[0]) < 500)):
@@ -219,32 +258,45 @@ def print_roster(event):
             else:
                 color = 'positive'
 
-            # if students are in the same class, dont' change the color
-            if(row[2] == prev_class):
-                even_odd -= 1
-            else:
-                header = row[:12]
-                if even_odd % 2 == 0:
-                    my_tree.insert(parent='', index='end', iid=k, text="", values=header, tags=('evenrow',color))
+            if((thesis.get() and (locale.atof(num[0]) == 499 or
+                                   locale.atof(num[0]) == 595 or
+                                   locale.atof(num[0]) == 599 or 
+                                   locale.atof(num[0]) == 695 or 
+                                   locale.atof(num[0]) == 699)) or 
+                                   thesis.get() == 0):
+                # if students are in the same class, dont' change the color
+                if(row[2] == prev_class):
+                    even_odd -= 1
                 else:
-                    my_tree.insert(parent='', index='end', iid=k, text="", values=header, tags=('oddrow',color))
-                first_row_in_class = k
-                k += 1
-                child = 0
-            
-            student_record = ['']*22
-            student_record[12:] = row[12:]
-            if even_odd % 2 == 0:
-                my_tree.insert(parent='', index='end', iid=k, text="", values=student_record, tags=('evenrow',color))
-            else:
-                my_tree.insert(parent='', index='end', iid=k, text="", values=student_record, tags=('oddrow',color))
+                    header = row[:12]
+                    if even_odd % 2 == 0:
+                        my_tree.insert(parent='', index='end', iid=k, text="", values=header, tags=('evenrow',color))
+                    else:
+                        my_tree.insert(parent='', index='end', iid=k, text="", values=header, tags=('oddrow',color))
+                    first_row_in_class = k
+                    k += 1
+                    child = 0
+                
+                student_record = ['']*22
+                student_record[12:] = row[12:]
+                if even_odd % 2 == 0:
+                    my_tree.insert(parent='', index='end', iid=k, text="", values=student_record, tags=('evenrow',color))
+                else:
+                    my_tree.insert(parent='', index='end', iid=k, text="", values=student_record, tags=('oddrow',color))
 
-            my_tree.move(k, first_row_in_class, child)
-            even_odd += 1
-            child += 1
+                my_tree.move(k, first_row_in_class, child)
+                even_odd += 1
+                child += 1
 
-            k+=1
-            prev_class = row[2]
+                k+=1
+                prev_class = row[2]
+                prev_student = row[12]
+                CRs += locale.atof(row[5])
+    # print the total number of credit hours for the semester
+    CRbox.configure(state='normal')
+    CRbox.delete(0,END)
+    CRbox.insert(0,str(CRs))
+    CRbox.configure(state='disabled')
 
 def update_dept(event):
     drop_student.current(0)
@@ -254,16 +306,6 @@ def update_dept(event):
 def clear_dept(event):
     drop_dept.current(0)
     print_roster(0)
-
-# define dept combo box
-drop_dept = ttk.Combobox(root,values = "", state="readonly", width=25)
-dept_label = Label(root, text="Dept.")
-dept_label.place(x=300, y=10)
-drop_dept.place(x=350, y=10)
-drop_dept.bind("<<ComboboxSelected>>", update_dept)
-drop_dept.bind("<Button-3>", clear_dept)
-drop_dept.bind("<Delete>", clear_dept)
-
 def update_student(event):
     drop_instruct.current(0)
     IDbox.delete(0, END)
@@ -271,16 +313,6 @@ def update_student(event):
 def clear_student(event):
     drop_student.current(0)
     print_roster(0)
-
-# define student combo box
-drop_student = ttk.Combobox(root,values = "", state="readonly", width=25)
-student_label = Label(root, text="Student")
-student_label.place(x=550, y=10)
-drop_student.place(x=600, y=10)
-drop_student.bind("<<ComboboxSelected>>", update_student)
-drop_student.bind("<Button-3>", clear_student)
-drop_student.bind("<Delete>", clear_student)
-
 def update_instruct(event):
     drop_student.current(0)
     IDbox.delete(0, END)
@@ -288,16 +320,6 @@ def update_instruct(event):
 def clear_instruct(event):
     drop_instruct.current(0)
     print_roster(0)
-
-# define instructor combo box
-drop_instruct = ttk.Combobox(root,values = "", state="readonly", width=25)
-instruct_label = Label(root, text="Instructor")
-instruct_label.place(x=790, y=10)
-drop_instruct.place(x=850, y=10)
-drop_instruct.bind("<<ComboboxSelected>>", update_instruct)
-drop_instruct.bind("<Button-3>", clear_instruct)
-drop_instruct.bind("<Delete>", clear_instruct)
-
 def updateID(event):
     drop_student.current(0)
     drop_instruct.current(0)
@@ -307,17 +329,61 @@ def clear_IDbox(event):
     IDbox.delete(0, END)
     print_roster(0)
 
+# define dept combo box
+drop_dept = ttk.Combobox(root,values = "", state="readonly", width=25)
+dept_label = Label(root, text="Dept.")
+dept_label.place(x=210, y=10)
+drop_dept.place(x=250, y=10)
+drop_dept.bind("<<ComboboxSelected>>", update_dept)
+drop_dept.bind("<Button-3>", clear_dept)
+drop_dept.bind("<Delete>", clear_dept)
+
+# define instructor combo box
+drop_instruct = ttk.Combobox(root,values = "", state="readonly", width=25)
+instruct_label = Label(root, text="Instructor")
+instruct_label.place(x=450, y=10)
+drop_instruct.place(x=510, y=10)
+drop_instruct.bind("<<ComboboxSelected>>", update_instruct)
+drop_instruct.bind("<Button-3>", clear_instruct)
+drop_instruct.bind("<Delete>", clear_instruct)
+
+# define student combo box
+drop_student = ttk.Combobox(root,values = "", state="readonly", width=25)
+student_label = Label(root, text="Student")
+student_label.place(x=720, y=10)
+drop_student.place(x=780, y=10)
+drop_student.bind("<<ComboboxSelected>>", update_student)
+drop_student.bind("<Button-3>", clear_student)
+drop_student.bind("<Delete>", clear_student)
+
 # define student ID combo box
+xplace = 10
 IDbox = ttk.Entry(root)
 ID_label = Label(root, text="Student ID")
-ID_label.place(x=1080, y = 10)
-IDbox.place(x=1150, y=10)
+#ID_label.place(x=xplace, y = 43)
+#IDbox.place(x=xplace+70, y=43)
+ID_label.place(x=RVwidth-250, y = 43)
+IDbox.place(x=RVwidth-185, width=100, y=43)
 IDbox.bind("<Button-3>", clear_IDbox)
 IDbox.bind("<Delete>", clear_IDbox)
 
 # define a search button
 IDbutton = ttk.Button(root, text="Search", command= lambda:updateID(0))
-IDbutton.place(x=1290, y=8)
+#IDbutton.place(x=xplace+210, y=38, width=60, height=30)
+IDbutton.place(x=RVwidth-75, y=38, width=60, height=30)
+
+# define CR tally
+CRbox = ttk.Entry(root, width=10)
+CR_label = Label(root, text="Total CRs")
+CR_label.place(x=RVwidth-250, y=10)
+CRbox.place(x=RVwidth-185, y=10)
+
+# define thesis checkbox
+thesis = IntVar()
+check = Checkbutton(root, text="include thesis",bd=0, variable = thesis, onvalue=1,
+                       offvalue=0, command = lambda: print_roster(0))
+check.place(x=RVwidth-115, y=10)
+
 
 # double-click on items to copy them to clipboard
 def select(event):
@@ -325,14 +391,30 @@ def select(event):
     col = re.findall(r'\d+', coltext)
     column = int(col[0])-1
     selected = my_tree.selection()
-    email = str('')
+    text = str('')
     for row in selected:
         values = my_tree.item(row, 'values')
-        email = email + str(values[column]) + '\n'
+        text = text + str(values[column]) + '\n'
     root.clipboard_clear()
-    root.clipboard_append(email)
+    root.clipboard_append(text)
     root.update()
 my_tree.bind('<Double-1>', select)
+
+# selection of rows
+def sum_rows(event):
+    if(len(my_tree.selection()) > 0):
+        CRs = 0
+        for item in my_tree.selection():
+            record = my_tree.item(item, 'values')
+            if(record[5] != ''):
+                CRs += locale.atof(record[5])*locale.atof(record[11])
+        # print the total number of credit hours for the semester
+        CRbox.configure(state='normal')
+        CRbox.delete(0,END)
+        CRbox.insert(0,str(CRs))
+        CRbox.configure(state='disabled')
+
+my_tree.bind("<<TreeviewSelect>>", sum_rows)
 
 #bind GUI resizing
 root.bind('<Configure>', resizer)
@@ -428,6 +510,7 @@ def save_file():
                 for idx_col, col in enumerate(values):
                     sheet.write(idx_row, idx_col, col)
                 idx_row += 1
+                prev_row = values
             workbook.close()
 
 # open button
